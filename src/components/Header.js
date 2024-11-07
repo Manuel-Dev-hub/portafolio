c7a47af02ec5118dca80d48755d1c76a2e5114af
Manuel Alejandro Rios Cardona <m.j.rios@18s.tech>
2024-11-06 19:42:46 -0600

    Descripción de los cambios

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUser, faTools, faAward, faEnvelope, faSun, faMoon, faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Header = ({ toggleTheme, darkMode }) => {
-  const [clicked, setClicked] = useState(null);
   const [isScrolled, setIsScrolled] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isMobileView, setIsMobileView] = useState(window.innerWidth <= 468);
+  const [isMobileView, setIsMobileView] = useState(window.innerWidth <= 768);
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 468);
 
   const handleScroll = () => {
     setIsScrolled(window.scrollY > 50);
@@ -15,51 +15,57 @@ const Header = ({ toggleTheme, darkMode }) => {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobileView(window.innerWidth <= 468);
+      setIsMobileView(window.innerWidth <= 768);
+      setIsSmallScreen(window.innerWidth <= 468);
     };
 
     window.addEventListener('scroll', handleScroll);
     window.addEventListener('resize', handleResize);
-    
+
     return () => {
       window.removeEventListener('scroll', handleScroll);
       window.removeEventListener('resize', handleResize);
     };
   }, []);
 
-  const handleClick = (link) => {
-    setClicked(link);
-    setTimeout(() => setClicked(null), 200);
-    setIsMenuOpen(false);
-  };
-
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const handleNavLinkClick = () => {
+    if (isMobileView) {
+      setIsMenuOpen(false); // Cierra el menú después de hacer clic en un enlace
+    }
+  };
+
   return (
     <header className="header">
-      <h1 className="header-title">{isMobileView ? 'Bienvenido' : 'Bienvenido a Mi Portafolio'}</h1>
-      <button className="menu-toggle" onClick={toggleMenu}>
+      <h1 className="header-title">
+        {isSmallScreen ? 'Bienvenido' : 'Bienvenido a Mi Portafolio'}
+      </h1>
+      <button
+        className={`menu-toggle ${darkMode ? 'dark-mode-button' : ''}`}
+        onClick={toggleMenu}
+      >
         <FontAwesomeIcon icon={faBars} />
       </button>
-      <nav className={`nav-bar ${isScrolled ? 'fixed' : ''} ${isMenuOpen ? 'open' : ''}`}>
-        <a href="#projects" onClick={() => handleClick('projects')}>
+      <nav className={`nav-bar ${isScrolled ? 'fixed' : ''} ${isMenuOpen ? 'open' : ''} ${darkMode ? 'dark-mode-nav' : ''}`}>
+        <a href="#projects" onClick={handleNavLinkClick}>
           <FontAwesomeIcon icon={faHome} /> Proyectos
         </a>
-        <a href="#about-me" onClick={() => handleClick('about-me')}>
+        <a href="#about-me" onClick={handleNavLinkClick}>
           <FontAwesomeIcon icon={faUser} /> Sobre Mí
         </a>
-        <a href="#skills" onClick={() => handleClick('skills')}>
+        <a href="#skills" onClick={handleNavLinkClick}>
           <FontAwesomeIcon icon={faTools} /> Habilidades
         </a>
-        <a href="#recognition" onClick={() => handleClick('recognition')}>
+        <a href="#recognition" onClick={handleNavLinkClick}>
           <FontAwesomeIcon icon={faAward} /> Reconocimientos
         </a>
-        <a href="#testimonials" onClick={() => handleClick('testimonials')}>
+        <a href="#testimonials" onClick={handleNavLinkClick}>
           <FontAwesomeIcon icon={faEnvelope} /> Testimonios
         </a>
-        <a href="#contact" onClick={() => handleClick('contact')}>
+        <a href="#contact" onClick={handleNavLinkClick}>
           <FontAwesomeIcon icon={faEnvelope} /> Contacto
         </a>
         <button className="theme-toggle" onClick={toggleTheme}>
